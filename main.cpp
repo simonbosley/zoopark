58ec3500baf3c40eccf76989a637a831f690da00
Simon Bosley <simon.bosley@gmail.com>
2012-07-08 13:03:35 +0100

    Adding header file info

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
-// This is how we launch our program!
+// File: main.cpp
+// Author: Simon Bosley
+// Copyright: Sunday 8th July, 2012
+// Synopsis: Program to launch our application
 
+// Standard Template Library includes
 #include <iostream>
 #include <string>
 
-using namespace std;
+// If we want to save sometime then uncomment the following line....
+//using namespace std;
 
 int main(char* argv[], int argc)
 {
